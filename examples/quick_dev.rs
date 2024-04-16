b6e8830a6eb597f96718f86b6dc27b4e2b477112
dauid64 <carlosdauid64br@gmail.com>
2024-04-16 15:06:51 -0300

    baseline

---
+++ examples/quick_dev.rs
@@ -0,0 +1,25 @@
+#![allow(unused)] // For beginning only.
+
+use anyhow::Result;
+use serde_json::json;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	let hc = httpc_test::new_client("http://localhost:8080")?;
+
+	hc.do_get("/index.html").await?.print().await?;
+
+	let req_login = hc.do_post(
+		"/api/login",
+		json!({
+			"username": "demo1",
+			"pwd": "welcome"
+		}),
+	);
+	req_login.await?.print().await?;
+
+	Ok(())
+}
+
+// cargo watch -q -c -w src/ -x run
+// cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
